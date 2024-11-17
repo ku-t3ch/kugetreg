6a8b56c1a95650a27d06e2a8be33d6b71d651151
Teerut26 <teerutsr@gmail.com>
2024-11-18 00:32:23 +0700

    Update Puppeteer launch options to use a fixed executable path for Chromium

--- src/configs/common/PuppeteerLaunchOptionsConfig.ts
+++ src/configs/common/PuppeteerLaunchOptionsConfig.ts
@@ -2,11 +2,12 @@ import { type PuppeteerLaunchOptions } from "puppeteer";
 
 export const PuppeteerLaunchOptionsConfig: PuppeteerLaunchOptions = {
   headless: false,
-  executablePath:
-    process.platform === "win32"
-      ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-      : process.platform === "linux"
-        ? "/usr/bin/chromium-browser"
-        : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  //   executablePath:
+  //     process.platform === "win32"
+  //       ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+  //       : process.platform === "linux"
+  //         ? "/usr/bin/chromium-browser"
+  //         : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+  executablePath: "/usr/bin/chromium-browser",
   args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
 };
