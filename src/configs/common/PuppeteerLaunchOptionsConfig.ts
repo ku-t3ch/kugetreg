906746338a9b2dd79ad3f14bb0282028b1b1454c
Teerut26 <teerutsr@gmail.com>
2024-11-18 00:32:41 +0700

    Update Puppeteer launch options to enable headless mode and set executable path based on platform

--- src/configs/common/PuppeteerLaunchOptionsConfig.ts
+++ src/configs/common/PuppeteerLaunchOptionsConfig.ts
@@ -1,13 +1,12 @@
 import { type PuppeteerLaunchOptions } from "puppeteer";
 
 export const PuppeteerLaunchOptionsConfig: PuppeteerLaunchOptions = {
-  headless: false,
-  //   executablePath:
-  //     process.platform === "win32"
-  //       ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-  //       : process.platform === "linux"
-  //         ? "/usr/bin/chromium-browser"
-  //         : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-  executablePath: "/usr/bin/chromium-browser",
+  headless: true,
+  executablePath:
+    process.platform === "win32"
+      ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+      : process.platform === "linux"
+        ? "/usr/bin/chromium-browser"
+        : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
   args: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"],
 };
