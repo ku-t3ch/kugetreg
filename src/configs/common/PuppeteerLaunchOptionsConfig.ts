b382e2ee6f2f466bb6d1ee45194cd7fd29dc9097
Teerut26 <teerutsr@gmail.com>
2024-11-23 15:59:22 +0700

    Update Puppeteer executable path configuration for cross-platform support

--- src/configs/common/PuppeteerLaunchOptionsConfig.ts
+++ src/configs/common/PuppeteerLaunchOptionsConfig.ts
@@ -15,6 +15,12 @@ import chromium from "@sparticuz/chromium";
 export const PuppeteerLaunchOptionsConfig: PuppeteerLaunchOptions = {
   args: process.env.IS_LOCAL ? ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"] : chromium.args,
   defaultViewport: chromium.defaultViewport,
-  executablePath: process.env.IS_LOCAL ? "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" : await chromium.executablePath(),
+  executablePath: process.env.IS_LOCAL
+    ? process.platform === "win32"
+      ? "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+      : process.platform === "linux"
+        ? "/usr/bin/chromium-browser"
+        : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+    : await chromium.executablePath(),
   headless: chromium.headless,
 };
