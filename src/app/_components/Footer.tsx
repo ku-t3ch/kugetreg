cf1cce8e622ba40676cc0668063a2372e621e73b
Teerut26 <teerutsr@gmail.com>
2024-11-24 16:13:56 +0700

    enhance sign-in page with loading state and update footer for GitHub link

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,7 +1,15 @@
 import { Text } from "@mantine/core";
+import { IconBrandGithub } from "@tabler/icons-react";
 
 export default function Footer() {
-  return (
-    <Text size="xs" c={"dimmed"}>KU Get Reg - {process.env.NEXT_PUBLIC_BUILD_MESSAGE ?? ""}</Text>
-  )
+    return (
+        <div className="flex gap-1">
+            <Text size="xs" c={"dimmed"}>KU Get Reg - {process.env.NEXT_PUBLIC_BUILD_MESSAGE ?? ""}</Text>
+            <Text size="xs" c={"dimmed"}>|</Text>
+            <a target="_blank" href="https://github.com/ku-t3ch/kugeterg" className="flex gap-1">
+                <Text size="xs" c={"dimmed"}>Open Source on</Text>
+                <Text size="xs" c={"dimmed"}><IconBrandGithub size={16} /></Text>
+            </a>
+        </div>
+    )
 }
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,74 +1,83 @@
 "use client";
 import { signIn } from "next-auth/react";
 import {
-  Alert,
-  Button,
-  Card,
-  Group,
-  PasswordInput,
-  Stack,
-  Text,
-  TextInput,
+    Alert,
+    Button,
+    Card,
+    Group,
+    PasswordInput,
+    Stack,
+    Text,
+    TextInput,
 } from "@mantine/core";
 import { IconLock, IconMail, IconUser } from "@tabler/icons-react";
 import { useParams } from "next/navigation";
 import Footer from "../_components/Footer";
+import { useState } from "react";
 
 export default function Page() {
-  const params = useParams<{ error: string }>();
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <Card maw={400} w={"100%"}>
-        <Stack>
-          <Text size="xl" fw={600}>
-            Sign In
-          </Text>
-          <form
-            className="flex flex-col gap-3"
-            onSubmit={async (e) => {
-              e.preventDefault();
-              const form = e.target as HTMLFormElement;
-              const username = (
-                form.elements.namedItem("username") as HTMLInputElement
-              ).value;
-              const password = (
-                form.elements.namedItem("password") as HTMLInputElement
-              ).value;
+    const params = useParams<{ error: string }>();
+    const [loading, setLoading] = useState(false);
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center">
+            <Card maw={400} w={"100%"}>
+                <Stack>
+                    <Text size="xl" fw={600}>
+                        Sign In
+                    </Text>
+                    <form
+                        className="flex flex-col gap-3"
+                        onSubmit={async (e) => {
+                            e.preventDefault();
+                            setLoading(true);
+                            const form = e.target as HTMLFormElement;
+                            const username = (
+                                form.elements.namedItem("username") as HTMLInputElement
+                            ).value;
+                            const password = (
+                                form.elements.namedItem("password") as HTMLInputElement
+                            ).value;
 
-              await signIn("credentials", {
-                username: username,
-                password: password,
-                callbackUrl: "/",
-              });
-            }}
-          >
-            {params.error && <Alert color="red">{params.error}</Alert>}
+                            try {
+                                setLoading(false);
+                                await signIn("credentials", {
+                                    username: username,
+                                    password: password,
+                                    callbackUrl: "/",
+                                });
+                            } catch (error) {
+                                setLoading(false);
+                                console.error(error);
+                            }
+                        }}
+                    >
+                        {params.error && <Alert color="red">{params.error}</Alert>}
 
-            <TextInput
-              size="md"
-              label="Username"
-              placeholder="เช่น b63xxxxxxxx หรือ regxxx"
-              name="username"
-              required
-              leftSection={<IconUser size={16} />}
-            />
-            <PasswordInput
-              size="md"
-              label="Password"
-              placeholder="รหัสผ่านบัญชีผู้ใช้เครือข่ายนนทรี"
-              required
-              name="password"
-              leftSection={<IconLock size={16} />}
-            />
-            <Button size="md" type="submit" fullWidth>
-              Sign in
-            </Button>
-            <Footer />
-          </form>
-        </Stack>
-      </Card>
-    </div>
-  );
+                        <TextInput
+                            size="md"
+                            label="Username"
+                            placeholder="เช่น b63xxxxxxxx หรือ regxxx"
+                            name="username"
+                            required
+                            leftSection={<IconUser size={16} />}
+                        />
+                        <PasswordInput
+                            size="md"
+                            label="Password"
+                            placeholder="รหัสผ่านบัญชีผู้ใช้เครือข่ายนนทรี"
+                            required
+                            name="password"
+                            leftSection={<IconLock size={16} />}
+                        />
+                        <Button loading={loading} loaderProps={{ size: "xs" }} size="md" type="submit" fullWidth>
+                            Sign in
+                        </Button>
+                        <Footer />
+                    </form>
+                </Stack>
+            </Card>
+        </div>
+    );
 }
 
 export const dynamic = "force-dynamic";
