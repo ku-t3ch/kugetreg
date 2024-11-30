cc3635834317117a6d72bd2f1bfb717d49c70ca9
Teerut26 <teerutsr@gmail.com>
2024-11-30 13:17:29 +0700

    fix: update firebase-admin and @types/node versions, enhance firebase initialization logic

--- src/configs/firebase/firebaseAdmin.ts
+++ src/configs/firebase/firebaseAdmin.ts
@@ -1,14 +1,16 @@
 import { env } from "@/env";
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(env.FIREBASE_ADMIN as string);
-const config = {
-  credential: admin.credential.cert(serviceAccount),
-};
 let firebaseAdmin: admin.app.App;
 
 if (!admin.apps.length) {
-  firebaseAdmin = admin.initializeApp(config);
+  firebaseAdmin = admin.initializeApp(
+    process.env.FIREBASE_ADMIN && process.env.FIREBASE_ADMIN.length > 0
+      ? {
+          credential: admin.credential.cert(JSON.parse(env.FIREBASE_ADMIN as string)),
+        }
+      : {},
+  );
 } else {
   firebaseAdmin = admin.app();
 }
