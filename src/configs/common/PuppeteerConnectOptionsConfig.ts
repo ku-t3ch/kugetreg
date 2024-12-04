64c6b8b3a9386126cd0cc6f1243d485b14513bd6
Teerut26 <teerutsr@gmail.com>
2024-12-04 10:41:14 +0700

    refactor: update Puppeteer connection options to use secure WebSocket endpoint

---
+++ src/configs/common/PuppeteerConnectOptionsConfig.ts
@@ -0,0 +1,5 @@
+import { type ConnectOptions } from "puppeteer-core";
+
+export const PuppeteerConnectOptionsConfig: ConnectOptions = {
+  browserWSEndpoint: "wss://browserless.teerut.com"
+};
--- src/configs/common/PuppeteerContectOptionsConfig.ts
+++
@@ -1,5 +0,0 @@
-import { type ConnectOptions } from "puppeteer-core";
-
-export const PuppeteerLaunchOptionsConfig: ConnectOptions = {
-  browserWSEndpoint: "ws://localhost:3000",
-};
