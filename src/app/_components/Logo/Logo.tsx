65871789df13b5269a8a99b278eb72a31cd3e7af
Teerut26 <teerutsr@gmail.com>
2024-12-07 10:59:42 +0700

    feat: wrap Logo component text in a Link for navigation

--- src/app/_components/Logo/Logo.tsx
+++ src/app/_components/Logo/Logo.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useMediaQuery } from "@mantine/hooks";
+import Link from "next/link";
 
 interface Props {
     element?: React.ReactNode;
@@ -9,8 +10,10 @@ interface Props {
 export default function Logo({ element }: Props) {
     const isMobile = useMediaQuery("(max-width: 768px)");
     return (
-        <span className="text-xl font-bold text-blue-600">
-            KU Get Reg {!isMobile && (element ?? ": จัดตารางเรียน")}
-        </span>
+        <Link href="/">
+            <span className="text-xl font-bold text-blue-600">
+                KU Get Reg {!isMobile && (element ?? ": จัดตารางเรียน")}
+            </span>
+        </Link>
     )
 }
