6c540c1e09b7f263a66c9dd32e947d4ef0016e0c
Teerut26 <teerutsr@gmail.com>
2024-12-12 16:42:31 +0700

    feat: enhance Footer component with Instagram link and improved layout

--- src/app/[locale]/_components/Footer.tsx
+++ src/app/[locale]/_components/Footer.tsx
@@ -1,14 +1,20 @@
-import { Text } from "@mantine/core";
-import { IconBrandGithub } from "@tabler/icons-react";
+import { Group, Text } from "@mantine/core";
+import { IconBrandGithub, IconBrandInstagram } from "@tabler/icons-react";
 
 export default function Footer() {
     return (
-        <div className="flex gap-1">
+        <div className="flex gap-1 flex-wrap">
             <Text size="xs" c={"dimmed"}>KU Get Reg - {process.env.NEXT_PUBLIC_BUILD_MESSAGE ?? ""}</Text>
             <Text size="xs" c={"dimmed"}>|</Text>
-            <a target="_blank" href="https://github.com/ku-t3ch/kugeterg" className="flex gap-1">
-                <Text size="xs" c={"dimmed"}>Open Source on</Text>
-                <Text size="xs" c={"dimmed"}><IconBrandGithub size={16} /></Text>
+            <a target="_blank" href="https://github.com/ku-t3ch/kugeterg">
+                <Text className="flex gap-1" size="xs" c={"dimmed"}>Open Source on <IconBrandGithub size={16} /></Text>
+            </a>
+            <Text size="xs" c={"dimmed"}>|</Text>
+            <a target="_blank" href="https://www.instagram.com/teerut_1t" className="flex gap-1">
+                <Text size="xs" c={"dimmed"}>แจ้งปัญหา</Text>
+                <Text c="blue" td="underline" size="xs" component={Group} gap={2}>
+                    <IconBrandInstagram size={16} /><Text>teerut_1t</Text>
+                </Text>
             </a>
         </div>
     )
