a0dc9d496a4a425a4507724736591457e91ef956
Teerut26 <teerutsr@gmail.com>
2024-12-14 20:07:06 +0700

    feat: enhance global error component with improved layout and error handling

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,20 +1,24 @@
 'use client' // Error boundaries must be Client Components
- 
+
 export default function GlobalError({
-  error,
-  reset,
+    error,
+    reset,
 }: {
-  error: Error & { digest?: string }
-  reset: () => void
+    error: Error & { digest?: string }
+    reset: () => void
 }) {
-  return (
-    // global-error must include html and body tags
-    <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <p>{error.message}</p>
-        <button onClick={() => reset()}>Try again</button>
-      </body>
-    </html>
-  )
+    return (
+        <html>
+            <body>
+                <div className="flex min-h-screen flex-col items-center justify-center bg-white">
+                    <div className="flex flex-col gap-3">
+                        <h2 className="text-xl font-bold">Something went wrong!</h2>
+                        <p className="font-bold">Error: <span className="font-normal text-red-600">{error.message}</span></p>
+                        <div className="flex gap-2">แจ้งปัญหามาที่ <a target="_blank" href="https://www.instagram.com/teerut_1t" className="font-bold text-blue-500 underline">teerut_1t</a></div>
+                        <button onClick={() => reset()} className="rounded-xl bg-sky-400 p-2 text-white">Try again</button>
+                    </div>
+                </div>
+            </body>
+        </html>
+    )
 }
